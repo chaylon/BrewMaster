c5063c76ce45f3c3d8eaf827761ace20f8ab1b04
chaylon <chaylon@fandm.edu>
2017-01-30 10:28:19 -0500

    started lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129194345) do
+ActiveRecord::Schema.define(version: 20170129230932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string  "name",        null: false
+    t.string  "description"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "last_name",                           null: false
