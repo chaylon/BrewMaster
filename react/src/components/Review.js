cb098d484ac34cbbbe9bf47a8600a9626287e2e2
chaylon <chaylon@fandm.edu>
2017-02-06 11:52:15 -0500

    finished delete for ratings

--- react/src/components/Review.js
+++ react/src/components/Review.js
@@ -3,8 +3,17 @@ import React from 'react';
 const Review = props => {
 
   let ratings = props.ratings.map((rating) => {
+    let handleDelete = () => {
+      props.handleDelete(props.beer.id, rating.id);
+    };
+
+    let deleteButton;
+
+    if (props.user.id === rating.user_id) {
+      deleteButton = <span><a href="javascript:;" onClick={handleDelete}>Delete</a></span>;
+    }
     return(
-      <div key={rating.id}>{rating.score} | {rating.review}</div>
+      <div key={rating.id}>{rating.score} | {rating.review} | {deleteButton}</div>
     );
   });
 
