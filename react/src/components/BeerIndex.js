9d69c3bc6f31d2e3ce4d7b99bf36674c9c714f12
chaylon <chaylon@fandm.edu>
2017-02-07 16:22:18 -0500

    search functionality complete

--- react/src/components/BeerIndex.js
+++ react/src/components/BeerIndex.js
@@ -1,75 +1,28 @@
 import React, {Component} from 'react';
 import Beer from './Beer';
 
-class BeerIndex extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      beers: [],
-      userLists: [],
-      currentUser: null
-    };
-    this.filterBeers = this.filterBeers.bind(this);
-  }
-
-  componentDidMount() {
-    this.filterBeers();
-  }
-
-  filterBeers() {
-    let name = this.props.searchName;
-    let brew = this.props.searchBrew;
-    let style = this.props.searchStyle;
-    fetch(`/api/v1/beers/filter?name_search=${name}&brew_search=${brew}&style_search=${style}`, {
-      credentials: "same-origin"
-    })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        let errorMessage = `${response.status}, (${response.statusText})`;
-        let error = new Error(errorMessage);
-        throw(error);
-      }
-    })
-    .then(response => response.json())
-    .then(body => {
-      let beers = body.beers;
-      let lists = body.lists;
-      let user = body.currentUser;
-      this.setState({
-        beers: beers,
-        userLists: lists,
-        currentUser: user
-      });
-    })
-    .catch(error => console.error(`Error in fetch: ${error.message}`));
-  }
-
-  render() {
-
-    let beers = this.state.beers.map((beer) => {
-
-      return(
-        <Beer
-          key = {beer.id}
-          beer = {beer}
-          user = {this.state.currentUser}
-          lists = {this.state.userLists}
-        />
-      );
-    });
-
-    beers = beers.sort(function(a,b) {
-      return a.key - b.key;
-    });
+const BeerIndex = props => {
+  let beers = props.beers.map((beer) => {
 
     return(
-      <div>
-        {beers}
-      </div>
+      <Beer
+        key = {beer.id}
+        beer = {beer}
+        user = {props.currentUser}
+        lists = {props.lists}
+      />
     );
-  }
-}
+  });
+
+  beers = beers.sort(function(a,b) {
+    return a.key - b.key;
+  });
+
+  return(
+    <div>
+      {beers}
+    </div>
+  );
+};
 
 export default BeerIndex;
--- react/src/components/SearchApp.js
+++ react/src/components/SearchApp.js
@@ -8,15 +8,26 @@ class SearchApp extends Component {
     this.state = {
       searchName: "",
       searchBrew: "",
-      searchStyle: ""
+      searchStyle: "",
+      beers: [],
+      lists: [],
+      currentUser: null
     };
     this.handleSearchName = this.handleSearchName.bind(this);
     this.handleSearchBrew = this.handleSearchBrew.bind(this);
     this.handleSearchStyle = this.handleSearchStyle.bind(this);
+    this.filterBeers = this.filterBeers.bind(this);
+  }
+
+  componentDidMount() {
+    this.filterBeers();
   }
 
   handleSearchName(term) {
-    this.setState({searchName: term});
+    this.setState({searchName: term}, () => {
+      this.filterBeers();
+      }
+    );
   }
 
   handleSearchBrew(term) {
@@ -25,6 +36,36 @@ class SearchApp extends Component {
 
   handleSearchStyle(term) {
     this.setState({searchStyle: term});
+  }
+
+  filterBeers() {
+    let name = this.state.searchName;
+    let brew = this.state.searchBrew;
+    let style = this.state.searchStyle;
+    fetch(`/api/v1/beers/filter?name_search=${name}&brew_search=${brew}&style_search=${style}`, {
+      credentials: "same-origin"
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status}, (${response.statusText})`;
+        let error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      let beers = body.beers;
+      let lists = body.lists;
+      let user = body.currentUser;
+      this.setState({
+        beers: beers,
+        userLists: lists,
+        currentUser: user
+      });
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
   render() {
@@ -39,6 +80,9 @@ class SearchApp extends Component {
           searchName = {this.state.searchName}
           searchBrew = {this.state.searchBrew}
           searchStyle = {this.state.searchStyle}
+          beers = {this.state.beers}
+          currentUser = {this.state.currentUser}
+          lists = {this.state.lists}
         />
       </div>
     );
