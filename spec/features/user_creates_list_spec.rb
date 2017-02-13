7d543c04c7f2fc9231a370ba81965ffd2fef4c3b
chaylon <chaylon@fandm.edu>
2017-02-12 22:56:50 -0500

    user and list feature tests

---
+++ spec/features/user_creates_list_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "user creates a list" do
+  scenario "successfully" do
+    user = FactoryGirl.create(:user)
+    login_as_user(user)
+    click_on "New List"
+
+    within("#myModal") do
+      fill_in "Name", with: "This is a new list"
+      click_on "Submit"
+    end
+
+    expect(page).to have_content("This is a new list")
+    expect(page).to have_content("Find new beers!")
+  end
+end
