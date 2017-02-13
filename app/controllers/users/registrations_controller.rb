7ab30f8123658b799437f7a72a133aec18d5bc7f
chaylon <chaylon@fandm.edu>
2017-02-12 21:49:07 -0500

    more tests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,9 +19,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    @new_list = List.new    
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -37,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -50,9 +51,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    current_user
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- spec/features/user_edits_account_spec.rb
+++ spec/features/user_edits_account_spec.rb
@@ -5,7 +5,37 @@ feature "users can edit their account" do
     user = FactoryGirl.create(:user)
     login_as_user(user)
 
-    visit edit_user_registration_path
-    
+    expect(page).to have_content("Welcome, first!")
+
+    click_on "Edit"
+
+    expect(page).to have_content("Edit User")
+
+    fill_in "First name", with: "Name"
+    fill_in "Current password", with: user.password
+    click_on "Update"
+
+    expect(page).to have_content("Welcome, Name!")
+  end
+
+  scenario "no password" do
+    user = FactoryGirl.create(:user)
+    login_as_user(user)
+    click_on "Edit"
+    fill_in "First name", with: "Name"
+    click_on "Update"
+
+    expect(page).to have_content("Current password can't be blank")
+  end
+
+  scenario "wrong password" do
+    user = FactoryGirl.create(:user)
+    login_as_user(user)
+    click_on "Edit"
+    fill_in "First name", with: "Name"
+    fill_in "Current password", with: "wrong"
+    click_on "Update"
+
+    expect(page).to have_content("Current password is invalid")
   end
 end
