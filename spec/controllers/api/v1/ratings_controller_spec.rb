baddb5b7f324751bcaf1c14a631c90a250c5fbf0
chaylon <chaylon@fandm.edu>
2017-02-17 15:59:15 -0500

    index tests

---
+++ spec/controllers/api/v1/ratings_controller_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+require "database_cleaner"
+
+describe Api::V1::RatingsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+  DatabaseCleaner.start
+
+  describe "GET #index" do
+    let!(:user) {FactoryGirl.create(:user)}
+    let!(:beer) {FactoryGirl.create(:beer)}
+    let!(:rating) {FactoryGirl.create(:rating, user: user, beer: beer)}
+
+    it "gets the rating information" do
+      sign_in(user)
+      get :index, params: {beer_id: beer.id}
+      json = JSON.parse(response.body)
+      
+      expect(json["ratings"][0]["score"]).to eq(rating.score)
+      expect(json["ratings"][0]["user_id"]).to eq(user.id)
+      expect(json["ratings"][0]["beer_id"]).to eq(beer.id)
+    end
+
+    it "calculates the score correctly from multiple ratings" do
+      user2 = FactoryGirl.create(:user)
+      rating2 = FactoryGirl.create(:rating, user: user2, beer: beer, score: 1)
+      sign_in(user)
+      get :index, params: {beer_id: beer.id}
+      json = JSON.parse(response.body)
+
+      expect(json["score"]).to eq(2)
+    end
+  end
+  DatabaseCleaner.clean
+end
