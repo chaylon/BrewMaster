1d954f279d464b171797657ae2a71f1f7cf154a2
chaylon <chaylon@fandm.edu>
2017-02-17 16:45:35 -0500

    create tests

--- spec/controllers/api/v1/ratings_controller_spec.rb
+++ spec/controllers/api/v1/ratings_controller_spec.rb
@@ -14,7 +14,7 @@ describe Api::V1::RatingsController, type: :controller do
       sign_in(user)
       get :index, params: {beer_id: beer.id}
       json = JSON.parse(response.body)
-      
+
       expect(json["ratings"][0]["score"]).to eq(rating.score)
       expect(json["ratings"][0]["user_id"]).to eq(user.id)
       expect(json["ratings"][0]["beer_id"]).to eq(beer.id)
@@ -28,6 +28,38 @@ describe Api::V1::RatingsController, type: :controller do
       json = JSON.parse(response.body)
 
       expect(json["score"]).to eq(2)
+    end
+  end
+
+  describe "POST #create" do
+    let!(:user) {FactoryGirl.create(:user)}
+    let!(:beer) {FactoryGirl.create(:beer)}
+
+    it "creates a rating" do
+      sign_in(user)
+
+      expect(Rating.all.length).to eq(0)
+
+      post :create, params: {beer_id: beer.id, user_id: user.id, rating: {beer_id: beer.id, user_id: user.id, score: 3}}
+
+      expect(Rating.all.length).to eq(1)
+
+      rating = Rating.first
+      expect(rating.beer_id).to eq(beer.id)
+      expect(rating.user_id).to eq(user.id)
+      expect(rating.score).to eq(3)
+    end
+
+    it "updates a rating" do
+      FactoryGirl.create(:rating, user: user, beer: beer)
+      sign_in(user)
+
+      expect(Rating.all.length).to eq(1)
+      expect(Rating.first.score).to eq(3)
+
+      post :create, params: {beer_id: beer.id, user_id: user.id, rating: {beer_id: beer.id, user_id: user.id, score: 2}}
+
+      expect(Rating.first.score).to eq(2)
     end
   end
   DatabaseCleaner.clean
