f2fcc069ecaeacddc0397a0040d48e17b1264854
chaylon <chaylon@fandm.edu>
2017-02-17 17:21:02 -0500

    some user show page tests

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -4,13 +4,12 @@ require "database_cleaner"
 feature "users show page" do
   DatabaseCleaner.start
   let!(:user) {FactoryGirl.create(:user)}
-  let!(:beer1) {FactoryGirl.create(:beer)}
-  let!(:beer2) {FactoryGirl.create(:beer)}
+  let!(:beer1) {FactoryGirl.create(:beer, style: "IPA")}
+  let!(:beer2) {FactoryGirl.create(:beer, style: "Stout")}
   let!(:rating1) {FactoryGirl.create(:rating, user: user, beer: beer1, score: 5)}
   let!(:rating2) {FactoryGirl.create(:rating, user: user, beer: beer2, score: 2)}
 
   scenario "favorite beers appear" do
-    binding.pry
     login_as_user(user)
     expect(page).to have_content(beer1.name)
   end
